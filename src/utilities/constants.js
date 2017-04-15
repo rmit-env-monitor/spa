eb1a3e8fab69be44c71af8675f497ae18c939c2e
joebui <dienbuijoe@outlook.com>
2017-04-15 16:01:36 +0700

    update production base_url

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -6,4 +6,4 @@ export const PUT = 'PUT'
 export const DELETE_METHOD = 'DELETE'
 export const BASE_URL = process.env.NODE_ENV === 'development' ?
     'http://localhost:3000' :
-    'https://evn-monitor.herokuapp.com'
+    'https://172.104.45.207'
