ce03ba321cef8a1dadad65f66def72dfa857128f
joebui <dienbuijoe@outlook.com>
2017-05-21 12:52:49 +0700

    change production base url

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -6,4 +6,4 @@ export const PUT = 'PUT'
 export const DELETE_METHOD = 'DELETE'
 export const BASE_URL = process.env.NODE_ENV === 'development' ?
     'http://localhost:3000' :
-    'http://sample-env.2ptfiw5dgm.ap-southeast-1.elasticbeanstalk.com'
+    'http://restful-service.ap-southeast-1.elasticbeanstalk.com'
