02e434c880b1a3045b8c9f90c6de65adb9408753
joebui <dienbuijoe@outlook.com>
2017-06-09 14:43:51 +0700

    setup sass implementation

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp')
 const concat = require('gulp-concat')
 const cleanCSS = require('gulp-clean-css')
+const sass = require('gulp-sass')
 
 gulp.task('min-css', () => {
     return gulp.src('dist/css/index.css')
@@ -9,4 +10,13 @@ gulp.task('min-css', () => {
         .pipe(gulp.dest('dist/css'))
 })
 
+gulp.task('sass', () => {
+    return gulp.src('./styles/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./dist/css'));
+})
+
 gulp.task('default', ['min-css'], () => { })
+gulp.task('sass:watch', () => {
+    gulp.watch('./styles/*.scss', ['sass']);
+})
