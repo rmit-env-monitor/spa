f688da220dcd4804c3416de8c6d9fa6aa6913e53
joebui <dienbuijoe@outlook.com>
2017-06-11 11:25:43 +0700

    change scss compile

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,13 @@
 const gulp = require('gulp')
-const concat = require('gulp-concat')
-const cleanCSS = require('gulp-clean-css')
 const sass = require('gulp-sass')
-
-gulp.task('min-css', () => {
-    return gulp.src('dist/css/index.css')
-        .pipe(cleanCSS())
-        .pipe(concat('index.min.css'))
-        .pipe(gulp.dest('dist/css'))
-})
 
 gulp.task('sass', () => {
     return gulp.src('./styles/*.scss')
-        .pipe(sass().on('error', sass.logError))
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
         .pipe(gulp.dest('./dist/css'));
 })
 
-gulp.task('default', ['min-css'], () => { })
+gulp.task('default', ['sass'], () => { })
 gulp.task('sass:watch', () => {
     gulp.watch('./styles/*.scss', ['sass']);
 })
