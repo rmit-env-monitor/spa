6d99bd1946d45d3e66dbf7f6e388092d10c6835a
joebui <dienbuijoe@outlook.com>
2017-12-16 17:43:33 +0700

    upgrade webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,73 @@
-const webpack = require("webpack")
-const path = require('path')
+const webpack = require("webpack");
+const path = require("path");
+const ExtractTextWebpackPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const config = {
-    entry: './src/index.jsx',
+  entry: [
+    "./src/index.jsx",
+    "./dist/css/index.scss",
+    "./dist/css/font-awesome-4.7.0/css/font-awesome.min.css"
+  ],
 
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'index.js',
-    },
+  output: {
+    path: path.resolve(__dirname, "./bundles"),
+    filename: "index.js",
+    publicPath: "/"
+  },
 
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: JSON.stringify('development')
-            }
-        })
-    ],
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("development")
+      }
+    }),
+    new ExtractTextWebpackPlugin({
+      filename: "index.css",
+      allChunks: true
+    }),
+    new HtmlWebpackPlugin({
+      template: "./dist/index.html",
+      filename: "index.html",
+      inject: "body"
+    })
+  ],
 
-    module: {
-        rules: [
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.(scss|css)$/,
+        loader: ExtractTextWebpackPlugin.extract({
+          use: [
             {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader'
+              loader: "css-loader"
+            },
+            {
+              loader: "resolve-url-loader"
+            },
+            {
+              loader: "sass-loader",
+              options: { sourceMap: true }
             }
-        ]
-    }
-}
+          ],
+          // use style-loader in development
+          fallback: "style-loader"
+        })
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2|svg)$/,
+        loader: "file-loader",
+        options: {
+          limit: 25000
+        }
+      }
+    ]
+  }
+};
 
-module.exports = config
+module.exports = config;
